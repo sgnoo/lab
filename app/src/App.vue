b7f248e6021a592910df6aa6ea7f2ccfe4cb283c
sgnoo <dn2757@gmail.com>
2021-06-21 00:20:22 +0900

    Add cute emoji 🤔

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app" style="margin: 36px;">
-    <router-link :to="'permit'">permit(EIP-712, EIP-2612)</router-link>
+    <div style="display: flex; align-items: center">
+      <router-link :to="'permit'">permit(EIP-712, EIP-2612)</router-link>
+      <img src="../public/thinking-face_1f914.png" alt="" width="20" height="20" style="margin-left: 4px;">
+    </div>
+
     <router-view />
   </div>
 </template>
